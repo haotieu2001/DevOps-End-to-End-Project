de2cc10ec063c0620f1ad8b11325fd0e53531e28
haotieu2001 <19521480@gm.uit.edu.vn>
2025-02-17 23:05:49 +0700

    add terraform remote backend

--- terraform/modules/remote-backend/variables.tf
+++ terraform/modules/remote-backend/variables.tf
@@ -1,9 +1,3 @@
-
-variable "region" {
-  description = "The AWS region to deploy resources in"
-  type        = string
-}
-
 variable "iam_user_name" {
   description = "The IAM user name for the backend module"
   type        = string
