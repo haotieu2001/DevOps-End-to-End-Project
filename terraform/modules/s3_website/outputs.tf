928f0fb66839e7cdc4d237dbf761891863120537
haotieu2001 <19521480@gm.uit.edu.vn>
2025-02-18 12:58:10 +0700

    add s3 website

---
+++ terraform/modules/s3_website/outputs.tf
@@ -0,0 +1,16 @@
+# bucket id and arn
+output "s3_bucket_id" {
+  description = "The ID of the S3 bucket created for the website"
+  value       = aws_s3_bucket.website_bucket.id
+}
+
+output "s3_bucket_arn" {
+  description = "The ARN of the S3 bucket created for the website"
+  value       = aws_s3_bucket.website_bucket.arn
+}
+
+output "website_url" {
+  description = "The URL of the website"
+  value       = "http://${aws_s3_bucket.website_bucket.bucket}.s3-website-${var.region}.amazonaws.com"
+}
+
--- terraform/modules/s3_website/variables.tf
+++ terraform/modules/s3_website/variables.tf
@@ -1,34 +1,3 @@
-
-variable "region" {
-  description = "The AWS region to deploy resources in"
-  type        = string
-}
-
-variable "iam_user_name" {
-  description = "The IAM user name for the backend module"
-  type        = string
-}
-
-variable "bucket_name" {
-  description = "The S3 bucket name for the backend module"
-  type        = string
-}
-
-variable "table_name" {
-  description = "The DynamoDB table name for the backend module"
-  type        = string
-}
-
-variable "root_domain" {
-  description = "The root domain for the Route53 hosted zone"
-  type        = string
-}
-
-variable "dns_record_ttl" {
-  description = "The TTL for the DNS record"
-  type        = number
-}
-
 variable "website_bucket" {
     description = "The name of the S3 bucket to create for the website"
     type        = string
@@ -51,3 +20,8 @@ variable "index_document" {
     type        = string
     default     = "index.html"
     }
+  
+variable "region" {
+    description = "The AWS region to create the S3 bucket in"
+    type        = string  
+}
